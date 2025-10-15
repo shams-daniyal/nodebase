5d895569b8153bedc86d133cf4c8d365f2258d57
daniyal <hussaindaniyal748@gmail.com>
2025-10-15 21:38:24 +0530

    project initialized

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
+
+const Page = () => {
+
+  const something = false;
+
+
+  return (
+    <div className="min-h-screen min-w-screen flex items-center justify-center">
+      <Button>
+        Click Me!
+      </Button>
+    </div>
+  )
+}
+
+export default Page;
